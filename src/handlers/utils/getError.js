e9832d0aee24ffdbbaf82a162feb7fcdd7a8e14f
d-vooolk <volk.dmitrii@otr.ru>
2025-03-15 18:19:36 +0300

    update settings

--- src/handlers/utils/getError.js
+++ src/handlers/utils/getError.js
@@ -1,5 +1,4 @@
 export const getError = (ctx, coinSymbol, error) => {
-    console.error('getError', ctx, coinSymbol, error);
     ctx.telegram.sendMessage(
         process.env.BOT_TOKEN,
         `❌ Ошибка при запросе данных для ${coinSymbol}.`,
