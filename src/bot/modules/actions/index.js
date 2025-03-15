ab03e1084d24a056b39fa76480f560ae79d4f2d2
d-vooolk <volk.dmitrii@otr.ru>
2025-03-15 04:06:07 +0300

    fix update button

--- src/bot/modules/actions/index.js
+++ src/bot/modules/actions/index.js
@@ -1,5 +1,4 @@
 import {
-  handleCoinPriceRequest,
   handleUpdateCallback
 } from "../../../handlers/handleCoinPriceRequest/handleCoinPriceRequest.js";
 
@@ -10,9 +9,8 @@ export const actions = {
 export const setupActions = (bot) => {
   bot.action(/^update_(.+)$/, async (ctx) => {
     try {
-      const symbol = ctx.match[1];
       await ctx.answerCbQuery("🔄 Обновляем данные...");
-      await handleCoinPriceRequest(ctx, symbol, true); // Передаем флаг обновления
+      await handleUpdateCallback(ctx);
     } catch (error) {
       console.error("Ошибка при обновлении:", error);
       await ctx.reply("❌ Ошибка при обновлении данных.");
