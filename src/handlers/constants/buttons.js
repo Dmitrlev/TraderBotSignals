eec4eeb13e2fd2dcbadddb28d4eacda400d7151f
d-vooolk <volk.dmitrii@otr.ru>
2025-03-16 18:00:14 +0300

    change 15m candles

--- src/handlers/constants/buttons.js
+++ src/handlers/constants/buttons.js
@@ -1,5 +1,5 @@
 export const timeframes = [
-  { label: "15m", interval: "15m", limit: 200 },
+  { label: "15m", interval: "15m", limit: 60 },
   { label: "1h", interval: "1h", limit: 100 },
   { label: "1d", interval: "1d", limit: 30 },
   {label: "🔄", interval: "15m", limit: 60},
