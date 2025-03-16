5faff8a2e707a443c38170827ba42e430483cc94
d-vooolk <volk.dmitrii@otr.ru>
2025-03-16 17:20:05 +0300

    change chart

--- src/handlers/handleCoinPriceRequest/generateCandlestickChart.js
+++ src/handlers/handleCoinPriceRequest/generateCandlestickChart.js
@@ -1,13 +1,9 @@
 import QuickChart from "quickchart-js";
-
-const binanceColors = {
-  up: "#2dbd85",
-  down: "#f6475d",
-  backgroundColor: '#17191d',
-};
+import {getSMA} from "../utils/getSMA.js";
+import {getCandleScale} from "../utils/getCandleScale.js";
+import {chartColors} from "../constants/chartColors.js";
 
 export async function generateChartURL(rawCandles) {
-
   const candles = rawCandles.map(c => ({
     x: c[0],
     o: parseFloat(c[1]),
@@ -16,48 +12,66 @@ export async function generateChartURL(rawCandles) {
     c: parseFloat(c[4]),
   }));
 
+  const sma = getSMA(candles, 10);
+  const [minX, maxX, offsetX] = getCandleScale(candles);
+
   const chart = new QuickChart();
   chart.setConfig({
     type: "candlestick",
     data: {
-      datasets: [{
-        data: candles,
-        color: {
-          up: binanceColors.up,
-          down: binanceColors.down,
+      datasets: [
+        {
+          data: candles,
+          color: {
+            up: chartColors.up,
+            down: chartColors.down,
+          },
+          borderColor: {
+            up: chartColors.up,
+            down: chartColors.down,
+          },
+          borderWidth: 1,
         },
-        borderColor: 'rgba(0,0,0,0.22)',
-        borderWidth: 0,
-      }]
+        {
+          type: "line",
+          data: sma,
+          borderColor: chartColors.smaColor,
+          borderWidth: 1.5,
+          fill: false,
+          pointRadius: 0,
+          tension: 0.2,
+        }
+      ]
     },
     options: {
+      responsive: true,
+      maintainAspectRatio: false,
       scales: {
         x: {
-          adapters: {
-            date: {
-              zone: 'UTC+3'
-            }
-          },
+          grid: { color: chartColors.gridColor },
+          ticks: { color: chartColors.textColor, autoSkip: false },
           time: {
             unit: "hour",
             stepSize: 6,
-            displayFormats: {
-              hour: "HH:mm",
-            }
+            displayFormats: { hour: "HH:mm" },
           },
-          ticks: {
-            autoSkip: false,
-          },
+          min: minX,
+          max: maxX + offsetX,
+        },
+        y: {
+          grid: { color: chartColors.gridColor },
+          ticks: { color: chartColors.textColor },
         },
       },
       plugins: {
-        legend: {display: false},
+        legend: { display: false },
       },
-      backgroundColor: binanceColors.backgroundColor,
+      backgroundColor: chartColors.backgroundColor,
     },
   });
 
-  chart?.setVersion("3");
+  chart.setBackgroundColor(chartColors.backgroundColor);
+  chart.setVersion("3");
 
-  return chart?.getUrl();
+  return chart.getUrl();
 }
